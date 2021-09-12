e5ba2a808ab6968b3f3439ae7e9d247f503007e7
imagire <imagire@gmail.com>
2021-09-12 16:06:56 +0900

    revert

---
+++ src/StaticLib/StaticLib.c
@@ -0,0 +1,10 @@
+﻿#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
+#include "Windows.h"                    // Windows API の機能定義
+
+#include "../include/library.h"
+
+const char *HelloWorld()
+{
+	// ここを何かに書き換える
+	return NULL;
+}
